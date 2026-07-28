19e2b2b6b786b43d4b43cc22d4ff99c819a0b714
Simar Malhotra <malhotrasimar009@gmail.com>
2026-07-28 12:07:57 -0400

    this is the single source of truth for all hotkeys and their desc
    Signed-off-by: Simar Malhotra <malhotrasimar009@gmail.com>

---
+++ src/commands.ts
@@ -0,0 +1,34 @@
+import type { Command } from "@codemirror/view";
+import { undo, redo } from "@codemirror/commands";
+import { openSearchPanel } from "@codemirror/search";
+import { toggleBold, toggleItalic, makeToggleHeading, insertLink } from "./formatting-commands";
+
+export interface PaletteCommand {
+  id: string;
+  label: string;
+  keys: string;
+  run: Command;
+}
+
+export function createPaletteCommands(fileInput: HTMLInputElement): PaletteCommand[] {
+  return [
+    { id: "bold", label: "Bold", keys: "Mod-b", run: toggleBold },
+    { id: "italic", label: "Italic", keys: "Mod-i", run: toggleItalic },
+    { id: "heading-1", label: "Heading 1", keys: "Mod-Alt-1", run: makeToggleHeading(1) },
+    { id: "heading-2", label: "Heading 2", keys: "Mod-Alt-2", run: makeToggleHeading(2) },
+    { id: "heading-3", label: "Heading 3", keys: "Mod-Alt-3", run: makeToggleHeading(3) },
+    { id: "insert-link", label: "Insert Link", keys: "Mod-Alt-k", run: insertLink },
+    {
+      id: "insert-image",
+      label: "Insert Image",
+      keys: "Mod-Shift-m",
+      run: () => {
+        fileInput.click();
+        return true;
+      },
+    },
+    { id: "undo", label: "Undo", keys: "Mod-z", run: undo },
+    { id: "redo", label: "Redo", keys: "Mod-Shift-z", run: redo },
+    { id: "find", label: "Find", keys: "Mod-f", run: openSearchPanel },
+  ];
+}
